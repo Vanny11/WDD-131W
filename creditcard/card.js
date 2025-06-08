46bc88ef550d659e63bdb06b8235699e888f5179
Vanny11 <vanessalemus93@gmail.com>
2025-06-07 21:56:43 -0600

    final

--- creditcard/card.js
+++ creditcard/card.js
@@ -1,5 +1,5 @@
 function isCardNumberValid(number) {
-    return number === "1234123412341234";
+    return number === '1234123412341234';
   }
   
   function displayError(msg) {
@@ -8,34 +8,34 @@ function isCardNumberValid(number) {
   
   function submitHandler(event) {
     event.preventDefault();
-    displayError("");
-  
-    const form = event.target;
-    const cardNumber = form.cardNumber.value;
-    const expMonth = parseInt(form.expMonth.value);
-    const expYear = parseInt(form.expYear.value);
-    let errorMsg = "";
+    displayError('');
+    
+    const cardNumber = this.cardNumber.value.replace(/\s/g, '');
+    const expMonth = this.expMonth.value;
+    const expYear = this.expYear.value;
+    let errorMsg = '';
   
     if (isNaN(cardNumber)) {
-      errorMsg += "Card number is not a number.\n";
+      errorMsg += 'Card number must be numeric.\n';
     } else if (!isCardNumberValid(cardNumber)) {
-      errorMsg += "Card number is not valid.\n";
+      errorMsg += 'Card number is not valid.\n';
     }
   
+    // Expiration date check
     const today = new Date();
-    const enteredDate = new Date(expYear, expMonth - 1);
-    if (enteredDate < today) {
-      errorMsg += "Card is expired.\n";
+    const inputDate = new Date(`20${expYear}`, expMonth - 1);
+    if (inputDate < today) {
+      errorMsg += 'Expiration date is not valid.\n';
     }
   
-    if (errorMsg !== "") {
+    if (errorMsg) {
       displayError(errorMsg);
       return false;
     }
   
-    alert("Form submitted successfully!");
+    alert('Form submitted successfully!');
     return true;
   }
   
-  document.querySelector("#credit-card").addEventListener("submit", submitHandler);
+  document.querySelector('#credit-card').addEventListener('submit', submitHandler);
   
